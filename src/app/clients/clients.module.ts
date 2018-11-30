66d32fd07cfabad5187261617b075dd1a5af6ade
Luis Aguilar <insomniocode@gmail.com>
2018-11-29 18:05:49 -0600

    new cheque components for clients

--- src/app/clients/clients.module.ts
+++ src/app/clients/clients.module.ts
@@ -20,6 +20,7 @@ import { CreateIdentityComponent } from './create-identity/create-identity.compo
 import { ClientsService } from './clients.service';
 import { UploadDocumentComponent } from './upload-document/upload-document.component';
 import { EditAddressComponent } from './edit-address/edit-address.component';
+import { ChequeComponent } from './cheque/cheque.component';
 
 @NgModule({
   imports: [
@@ -42,7 +43,8 @@ import { EditAddressComponent } from './edit-address/edit-address.component';
     CreateAddressComponent,
     CreateIdentityComponent,
     UploadDocumentComponent,
-    EditAddressComponent
+    EditAddressComponent,
+    ChequeComponent
 ],
   providers: [
     ClientsService,
